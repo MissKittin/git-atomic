386bcf94eacf88d2cd1c925bf68c2973453f7bc9
MissKittin <you@example.com>
2020-03-12 23:00:22 +0000

    Scripts improvements

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,70 @@
 #!/bin/sh
+pre_deploy_hook()
+{
+	# commands before deploy
+	echo -n ''
+}
+post_deploy_hook()
+{
+	# commands after deploy
+	echo -n ''
+}
+
+if [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
+	echo 'deploy.sh `new_release_version`'
+	echo 'deploy.sh rollback `previous_version`'
+	echo 'deploy.sh clean'
+	exit 0
+fi
+
+# Check environment
+if [ ! -e dev ]; then
+	echo 'environment not initialized'
+	exit 1
+fi
+
+# Rollback part
+if [ "$1" = 'rollback' ]; then
+	if [ "$2" = '' ]; then
+		echo 'no version'
+		exit 1
+	fi
+	if [ ! -e releases ]; then
+		echo 'this is first deploy'
+		exit 1
+	fi
+	if [ ! -e releases/$2 ]; then
+		echo 'this version not exists'
+		exit 1
+	fi
+
+	pre_deploy_hook
+	rm current > /dev/null 2>&1
+	ln -s ./releases/$2 current
+	post_deploy_hook
+
+	echo 'OK'
+	exit 0
+fi
+
+# Clean part
+if [ "$1" = 'clean' ]; then
+	ls -t | tail -n +11 | xargs rm -r -f > /dev/null 2>&1 && echo 'OK'
+	exit 0
+fi
+
+# Main part
 if [ "$1" = '' ]; then
 	echo 'no version'
 	exit 1
 fi
 
+pre_deploy_hook
 [ -e releases ] || mkdir releases
 cp -rfp dev ./releases/$1
 rm current > /dev/null 2>&1
 ln -s ./releases/$1 current
+post_deploy_hook
 
 echo 'OK'
 exit 0
--- init.sh
+++ init.sh
@@ -1,15 +1,59 @@
 #!/bin/sh
-mkdir dev
-cd dev
-git init
-git config core.worktree ..
-git config receive.denycurrentbranch ignore
-cat << EOF > .git/hooks/post-receive
+# Functions
+
+gitAtomic_createHook()
+{ # Customize below
+cat << EOF > ./.git/hooks/post-receive
 #!/bin/sh
 git checkout -f
 EOF
 chmod 755 ./.git/hooks/post-receive
-cd ..
-#rm init.sh
-rm README.TXT
+}
+
+gitAtomic_firstTimeInit()
+{
+	if [ -e 'dev' ]; then
+		echo 'dev repo exists, use `renew`'
+		exit 1
+	fi
+	mkdir ./dev
+	cd ./dev
+	git init
+	git config core.worktree ..
+	git config receive.denycurrentbranch ignore
+	gitAtomic_createHook
+	cd ..
+	rm README.TXT
+}
+
+gitAtomic_renew()
+{
+	rm -r -f ./dev
+	mkdir ./dev
+	cd ./dev
+	git init
+	git config core.worktree ..
+	git config receive.denycurrentbranch ignore
+	gitAtomic_createHook
+	cd ..
+}
+
+# Help
+if [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
+	echo 'init.sh'
+	echo 'init.sh renew'
+	exit 0
+fi
+
+# Renew
+if [ "$1" = 'renew' ]; then
+	echo -n 'Sure? (y/[n]) '
+	read answer
+	[ "$answer" = 'y' ] && gitAtomic_renew
+	exit 0
+fi
+
+# First-time init
+gitAtomic_firstTimeInit
+
 exit 0
